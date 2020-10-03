115cca3b6c2b54d2146783ac302e9ad0546186ed
marcelo <marceloarthurb@gmail.com>
2020-10-03 10:19:09 -0500

    update

---
+++ test.js
@@ -0,0 +1,12 @@
+const { expect } = require("chai");
+const assert = require('assert');
+
+
+describe('Simple Math Test', () => {
+    it('should return 2', () => {
+           assert.strictEqual(1 + 1, 2);
+       });
+    it('should return 9', () => {
+           assert.strictEqual(3 * 3, 9);
+       });
+   });
